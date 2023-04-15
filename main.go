c3991b8ba0650310b3dfe0b172a87c1c6e3927c3
thanapongr <thanapong.rodruang@gmail.com>
2023-04-15 09:02:42 +0700

    First routing

--- main.go
+++ main.go
@@ -7,10 +7,16 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+func getAllMoviesHandler(c echo.Context) error {
+	return c.JSON(200, []string{"movie1", "movie2"})
+}
+
 func main() {
 	fmt.Println("Welcome to iCinema")
 
 	e := echo.New()
+
+	e.GET("/movies", getAllMoviesHandler)
 
 	port := "80"
 	log.Println("Start at port:" + port)
