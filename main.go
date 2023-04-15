8c0c763c4778173d063b548e01b6bb666835138a
thanapongr <thanapong.rodruang@gmail.com>
2023-04-15 09:09:21 +0700

    Add movies struct

--- main.go
+++ main.go
@@ -3,12 +3,31 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+type Movie struct {
+	ImdbID      string  `json:"imdbID"`
+	Title       string  `json:"titel"`
+	Year        int     `json:"year"`
+	Rating      float32 `json:"rating"`
+	IsSuperHero bool    `json:"isSuperHero"`
+}
+
+var movies = []Movie{
+	{
+		ImdbID:      "tt4154796",
+		Title:       "Avenger: Endgame",
+		Year:        2019,
+		Rating:      8.4,
+		IsSuperHero: true,
+	},
+}
+
 func getAllMoviesHandler(c echo.Context) error {
-	return c.JSON(200, []string{"movie1", "movie2"})
+	return c.JSON(http.StatusOK, movies)
 }
 
 func main() {
