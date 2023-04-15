24187481891b7d077f2c193a1799647c22709b0f
thanapongr <thanapong.rodruang@gmail.com>
2023-04-15 09:29:29 +0700

    Add query func

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -27,7 +28,39 @@ var movies = []Movie{
 }
 
 func getAllMoviesHandler(c echo.Context) error {
-	return c.JSON(http.StatusOK, movies)
+	y := c.QueryParam("year")
+
+	if y == "" {
+		return c.JSON(http.StatusOK, movies)
+	}
+
+	year, err := strconv.Atoi(y)
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	ms := []Movie{}
+
+	for _, m := range movies {
+		if year == m.Year {
+			ms = append(ms, m)
+		}
+	}
+
+	return c.JSON(http.StatusOK, ms)
+}
+
+func getMovieByIdHandler(c echo.Context) error {
+	id := c.Param("id")
+
+	for _, m := range movies {
+		if m.ImdbID == id {
+			return c.JSON(http.StatusOK, m)
+		}
+	}
+
+	return c.JSON(http.StatusNotFound, map[string]string{"message": "not found"})
 }
 
 func main() {
@@ -36,6 +69,7 @@ func main() {
 	e := echo.New()
 
 	e.GET("/movies", getAllMoviesHandler)
+	e.GET("/movies/:id", getMovieByIdHandler)
 
 	port := "80"
 	log.Println("Start at port:" + port)
